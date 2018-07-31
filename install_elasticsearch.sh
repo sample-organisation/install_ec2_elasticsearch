4d5d4fb41e0a03e4f2b890b8339583a017585a6b
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-07-31 16:29:22 +0530

    Adds elasticsearch service start

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -6,3 +6,4 @@ apt-get install -y default-jdk
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
 dpkg -i elasticsearch-2.3.1.deb
 systemctl enable elasticsearch.service
+systemctl start elasticsearch
